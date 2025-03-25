9647f9496c97eb13d84656d76b8971f485b5dda4
Hiral Garg <hiralgarg0501@gmail.com>
2025-03-25 23:47:43 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -3,5 +3,11 @@ int main(){
     int a,b;
     char op;
     scanf("%d %d %c",&a, &b, &op);
-    
+    switch(op){
+        case'+': printf("%d",a+b);
+                 break; 
+        case'-': printf("%d",a-b);
+                break;
+    }
+
 }
